24021073adfe92d6a7a42a2767db11c545397c06
leyiqiang <leyiqiang@gmail.com>
2019-12-05 21:28:44 +0800

    monitor

--- make-run.sh
+++ make-run.sh
@@ -5,7 +5,7 @@
 # export DISPLAY=:0 #needed if you are running a simple gui app.
 
 process=/home/ubuntu/pypcap-monitor/sniff.py
-makerun="sudo python3 /home/ubuntu/pypcap-monitor/sniff.py"
+makerun="sudo python3 /home/ubuntu/pypcap-monitor/sniff.py -c config.yml"
 logfile=/home/ubuntu/log_running.txt
 while true
 do
